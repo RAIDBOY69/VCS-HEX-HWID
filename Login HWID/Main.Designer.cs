06fe1a570fcc3e634862f013d458f8d3648f6f24
RAIDBOY <vcshex@gmail.com>
2023-05-10 22:36:58 +0700

    added error window and error message

--- Login HWID/Main.Designer.cs
+++ Login HWID/Main.Designer.cs
@@ -29,90 +29,34 @@ namespace Login_HWID
         /// </summary>
         private void InitializeComponent()
         {
-            this.Password = new Guna.UI2.WinForms.Guna2TextBox();
-            this.Username = new Guna.UI2.WinForms.Guna2TextBox();
             this.LoginBTN = new Guna.UI2.WinForms.Guna2Button();
             this.SuspendLayout();
-            // 
-            // Password
-            // 
-            this.Password.BorderRadius = 10;
-            this.Password.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.Password.DefaultText = "Password";
-            this.Password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.Password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.Password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.Password.DisabledState.Parent = this.Password;
-            this.Password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.Password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.Password.FocusedState.Parent = this.Password;
-            this.Password.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
-            this.Password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.Password.HoverState.Parent = this.Password;
-            this.Password.Location = new System.Drawing.Point(95, 73);
-            this.Password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Password.Name = "Password";
-            this.Password.PasswordChar = '\0';
-            this.Password.PlaceholderText = "";
-            this.Password.SelectedText = "";
-            this.Password.SelectionStart = 8;
-            this.Password.ShadowDecoration.Parent = this.Password;
-            this.Password.Size = new System.Drawing.Size(204, 28);
-            this.Password.TabIndex = 7;
-            this.Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
-            // 
-            // Username
-            // 
-            this.Username.BorderRadius = 10;
-            this.Username.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.Username.DefaultText = "Username";
-            this.Username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.Username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.Username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.Username.DisabledState.Parent = this.Username;
-            this.Username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.Username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.Username.FocusedState.Parent = this.Username;
-            this.Username.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
-            this.Username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.Username.HoverState.Parent = this.Username;
-            this.Username.Location = new System.Drawing.Point(95, 37);
-            this.Username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Username.Name = "Username";
-            this.Username.PasswordChar = '\0';
-            this.Username.PlaceholderText = "";
-            this.Username.SelectedText = "";
-            this.Username.SelectionStart = 8;
-            this.Username.ShadowDecoration.Parent = this.Username;
-            this.Username.Size = new System.Drawing.Size(204, 28);
-            this.Username.TabIndex = 6;
-            this.Username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
             // 
             // LoginBTN
             // 
             this.LoginBTN.BorderRadius = 10;
             this.LoginBTN.CheckedState.Parent = this.LoginBTN;
             this.LoginBTN.CustomImages.Parent = this.LoginBTN;
+            this.LoginBTN.FillColor = System.Drawing.Color.Red;
             this.LoginBTN.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.LoginBTN.ForeColor = System.Drawing.Color.White;
             this.LoginBTN.HoverState.Parent = this.LoginBTN;
-            this.LoginBTN.Location = new System.Drawing.Point(109, 118);
+            this.LoginBTN.Location = new System.Drawing.Point(-6, -4);
             this.LoginBTN.Name = "LoginBTN";
             this.LoginBTN.ShadowDecoration.Parent = this.LoginBTN;
-            this.LoginBTN.Size = new System.Drawing.Size(180, 34);
+            this.LoginBTN.Size = new System.Drawing.Size(402, 195);
             this.LoginBTN.TabIndex = 5;
-            this.LoginBTN.Text = "Login";
+            this.LoginBTN.Text = "ERROR CONTACT ADMIN @ezvocabsize";
             this.LoginBTN.Click += new System.EventHandler(this.LoginBTN_Click);
             // 
             // Main
             // 
             this.ClientSize = new System.Drawing.Size(395, 189);
-            this.Controls.Add(this.Password);
-            this.Controls.Add(this.Username);
             this.Controls.Add(this.LoginBTN);
+            this.MaximumSize = new System.Drawing.Size(411, 228);
+            this.MinimumSize = new System.Drawing.Size(411, 228);
             this.Name = "Main";
+            this.Text = "Error";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
@@ -120,9 +64,6 @@ namespace Login_HWID
 
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2TextBox Password;
-        private Guna.UI2.WinForms.Guna2TextBox Username;
         private Guna.UI2.WinForms.Guna2Button LoginBTN;
     }
 }
--- Login HWID/Main.cs
+++ Login HWID/Main.cs
@@ -56,140 +56,135 @@ namespace Login_HWID
             string title = GenerateRandomString();
             Console.Title = title;
             Console.WriteLine("[+]Initializing Driver");
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            _webDriver = new ChromeDriver();
-            Thread.Sleep(3000);
-            #endregion
-
-
-            #region ChromeDriver Settings
-            //ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-            //service.EnableVerboseLogging = false;
-            //service.SuppressInitialDiagnosticInformation = true;
-            //service.HideCommandPromptWindow = true;
-
-            //ChromeOptions options = new ChromeOptions();
-
-            //options.PageLoadStrategy = PageLoadStrategy.Normal;
-
-
-
-            //options.AddArgument("--disable-logging");
-            //options.AddArgument("--disable-dev-shm-usage");
-            //options.AddArgument("--log-level=3");
-            //options.AddArgument("--output=/dev/null");
-            #endregion
-            #region ChromeDriver Launch Check
-            IWebDriver driver2 = _webDriver;
-            IWebDriver driver1 = driver2;
-            driver2.Navigate().GoToUrl("https://vocabsize-legacy.xeersoft.co.th/");
-
-            IWebElement WaitUntilElementExists(By elementLocator, int timeout = 10)
+            try
             {
-                try
+                new DriverManager().SetUpDriver(new ChromeConfig());
+                _webDriver = new ChromeDriver();
+                Thread.Sleep(3000);
+                #endregion
+
+
+                #region ChromeDriver Settings
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                service.EnableVerboseLogging = false;
+                service.SuppressInitialDiagnosticInformation = true;
+                service.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                //options.PageLoadStrategy = PageLoadStrategy.Normal;
+                options.AddArgument("--disable-logging");
+                options.AddArgument("--disable-dev-shm-usage");
+                options.AddArgument("--log-level=3");
+                options.AddArgument("--output=/dev/null");
+                #endregion
+                #region ChromeDriver Launch Check
+                IWebDriver driver2 = _webDriver;
+                IWebDriver driver1 = driver2;
+                Console.Clear();
+                driver2.Navigate().GoToUrl("https://vocabsize-legacy.xeersoft.co.th/");
+
+                IWebElement WaitUntilElementExists(By elementLocator, int timeout = 10)
                 {
-                    var waitt = new WebDriverWait(driver1, TimeSpan.FromSeconds(timeout));
-                    return waitt.Until(ExpectedConditions.ElementExists(elementLocator));
+                    try
+                    {
+                        var waitt = new WebDriverWait(driver1, TimeSpan.FromSeconds(timeout));
+                        return waitt.Until(ExpectedConditions.ElementExists(elementLocator));
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
+                        throw;
+                    }
                 }
-                catch (NoSuchElementException)
+
+                //VERSION
+                Console.Write(".");
+                Console.Clear();
+                Console.WriteLine("OK!");
+                Thread.Sleep(1000);
+                Console.Clear();
+                #endregion
+                #region VCSBOT
+
+
+                string usernamer;
+                string passwordr;
+                Console.WriteLine("\n \nEnter enconcept username: ");
+                usernamer = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("\n \nEnter enconcept password: ");
+                passwordr = Console.ReadLine();
+                driver1.FindElement(By.Id("txt_email")).SendKeys(usernamer);
+                driver1.FindElement(By.Id("txt_password")).SendKeys(passwordr);
+                driver1.FindElement(By.Id("btn_submit")).Click();
+
+
+                WebDriverWait wait = new WebDriverWait(driver1, TimeSpan.FromSeconds(20));
+                _ = wait.Until(condition: driver =>
                 {
-                    Console.WriteLine("Element with locator: '" + elementLocator + "' was not found in current context page.");
-                    throw;
+                    return driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div/div[1]/div/a/img"));
+                });
+
+                driver1.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div/div[1]/div/a/img")).Click();
+
+                string assignmentnum;
+                Console.Clear();
+                Console.WriteLine("\nEnter TEST No.");
+                assignmentnum = Console.ReadLine();
+                var assignmentnum2 = String.Format("/html/body/div[2]/table/tbody/tr[{0}]/td[7]", assignmentnum); ///3 row
+                                                                                                                  ///html/body/div[2]/table/tbody/tr[{0}]/td[7]
+                //var practicenum = String.Format("/html/body/div[2]/table/tbody/tr[{0}]/td[7]/a[1]", assignmentnum);
+
+
+                string xpath = assignmentnum2 + "//a[contains(text(), 'Vocab Test')]";
+                driver1.FindElement(By.XPath(xpath)).Click();
+
+                //*[@id="result_list"]/table/tbody/tr[1]/td[7]/a[2]
+
+
+                driver1.FindElement(By.Id("btn_skip")).Click();
+                driver1.FindElement(By.Id("button1")).Click();
+
+                //word amount
+                Console.Clear();
+                int word_amount = Convert.ToInt16(driver1.FindElement(By.XPath("/html/body/section/div[1]/div/div/div[1]/div[6]/span")).GetAttribute("innerHTML"));
+                Console.WriteLine("\n[*]Total " + word_amount + " Words");
+
+
+
+                for (int i = 0; i < word_amount; i++)
+                {
+                    int amount = i;
+                    var wd = String.Format("/html/body/section/div[4]/table/tbody/tr[{0}]/td[3]/span[1]", amount + 1); // 2 row
+                    var words = (driver1.FindElement(By.XPath(wd)).GetAttribute("innerHTML"));
+                    var txtbox = String.Format("/html/body/section/div[4]/table/tbody/tr[{0}]/td[4]/div/input", amount + 1);
+                    driver1.FindElement(By.XPath(txtbox)).SendKeys(words);
                 }
+                ///SAVE
+                IWebElement ele10 = driver1.FindElement(By.XPath("//*[@id=\"btn_save\"]"));
+                ele10.Click();
+
+                Console.WriteLine("[+]Assignment Complete");
+                Thread.Sleep(1500);
+                Console.WriteLine("[+]Closing WebDriver.");
+                Thread.Sleep(1500);
+                //close the browser
+                _webDriver.Quit();
+                Console.WriteLine("[+]Terminating Program..");
+                Thread.Sleep(1500);
+                Environment.Exit(0);
+                #endregion
+            
             }
-
-            //VERSION
-            Console.Write(".");
-            Console.Clear();
-            Console.WriteLine("OK!");
-            Thread.Sleep(1000);
-            Console.Clear();
-            #endregion
-            #region VCSBOT
-
-
-            string usernamer;
-            string passwordr;
-            Console.WriteLine("\n \nEnter enconcept username: ");
-            usernamer = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("\n \nEnter enconcept password: ");
-            passwordr = Console.ReadLine();
-            driver1.FindElement(By.Id("txt_email")).SendKeys(usernamer);
-            driver1.FindElement(By.Id("txt_password")).SendKeys(passwordr);
-            driver1.FindElement(By.Id("btn_submit")).Click();
-
-
-            WebDriverWait wait = new WebDriverWait(driver1, TimeSpan.FromSeconds(20));
-            _ = wait.Until(condition: driver =>
+            catch(Exception ex)
             {
-                return driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div/div[1]/div/a/img"));
-            });
-
-            driver1.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div/div[1]/div/a/img")).Click();
-
-            string assignmentnum;
-            Console.Clear();
-            Console.WriteLine("\nEnter TEST No.");
-            assignmentnum = Console.ReadLine();
-            var assignmentnum2 = String.Format("/html/body/div[2]/table/tbody/tr[{0}]/td[7]", assignmentnum); ///3 row
-                                                                                                              ///html/body/div[2]/table/tbody/tr[{0}]/td[7]
-            //var practicenum = String.Format("/html/body/div[2]/table/tbody/tr[{0}]/td[7]/a[1]", assignmentnum);
-
-
-            string xpath = assignmentnum2 + "//a[contains(text(), 'Vocab Test')]";
-            driver1.FindElement(By.XPath(xpath)).Click();
-
-            //*[@id="result_list"]/table/tbody/tr[1]/td[7]/a[2]
-
-
-            driver1.FindElement(By.Id("btn_skip")).Click();
-            driver1.FindElement(By.Id("button1")).Click();
-
-            //word amount
-            Console.Clear();
-            int word_amount = Convert.ToInt16(driver1.FindElement(By.XPath("/html/body/section/div[1]/div/div/div[1]/div[6]/span")).GetAttribute("innerHTML"));
-            Console.WriteLine("\n[*]Total " + word_amount + " Words");
-
-
-
-            for (int i = 0; i < word_amount; i++)
-            {
-                int amount = i;
-                var wd = String.Format("/html/body/section/div[4]/table/tbody/tr[{0}]/td[3]/span[1]", amount + 1); // 2 row
-                var words = (driver1.FindElement(By.XPath(wd)).GetAttribute("innerHTML"));
-                var txtbox = String.Format("/html/body/section/div[4]/table/tbody/tr[{0}]/td[4]/div/input", amount + 1);
-                driver1.FindElement(By.XPath(txtbox)).SendKeys(words);
+                Console.Clear();
+                Console.WriteLine("[X]Error: " + ex.Message + " Please contact ADMIN @ezvocabsize.");
             }
-            ///SAVE
-            IWebElement ele10 = driver1.FindElement(By.XPath("//*[@id=\"btn_save\"]"));
-            ele10.Click();
-
-            Console.WriteLine("[+]Assignment Complete");
-            Thread.Sleep(1500);
-            Console.WriteLine("[+]Closing WebDriver.");
-            Thread.Sleep(1500);
-            //close the browser
-            _webDriver.Quit();
-            Console.WriteLine("[+]Terminating Program..");
-            Thread.Sleep(1500);
-            Environment.Exit(0);
-            #endregion
-        }
-
-        private void Username_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Password_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+            }
 
         private void LoginBTN_Click(object sender, EventArgs e)
         {
-
+            System.Diagnostics.Process.Start("https://www.instagram.com/ezvocabsize/");
         }
     }
 }
